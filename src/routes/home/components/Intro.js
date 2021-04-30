e23c172f1913825b59f816836685c4489ec7c510
Max Huckstepp <mhuckstepp@gmail.com>
2021-04-30 07:22:51 -0700

    work on login and register

--- src/routes/home/components/Intro.js
+++ src/routes/home/components/Intro.js
@@ -1,21 +1,47 @@
-import React from 'react'
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
 
 const Intro = () => {
-  return (
-  <section id="one">
-    <header className="major">
-      <h2>Anyone can go out and buy a cookbook,<br />
-        but you want a place to store all your<br /> 
-        secret family recipes, handed down<br/>
-        from generation to generation.</h2>
-    </header>
-    <p>The little cards grandma wrote her recipes on in her beautiful cursive are getting lost or are hard to read. You need somewhere secure to keep my recipes with me forever!</p>
-    <ul className="actions">
-      <li><a href="\#" className="button">Register Here</a></li>
-      <li><a href="\#" className="button primary">Log In</a></li>
-    </ul>
-  </section>
-  )
-}
+  // Links to register and login not loading on first try with link tags or history.push
+  // let history = useHistory();
 
-export default Intro
+  // const pushToLogin = () => {
+  //   history.push("/login");
+  // };
+
+  return (
+    <section id="one">
+      <header className="major">
+        <h2>
+          Anyone can go out and buy a cookbook,
+          <br />
+          but you want a place to store all your
+          <br />
+          secret family recipes, handed down
+          <br />
+          from generation to generation.
+        </h2>
+      </header>
+      <p>
+        The little cards grandma wrote her recipes on in her beautiful cursive
+        are getting lost or are hard to read. You need somewhere secure to keep
+        my recipes with me forever!
+      </p>
+      <ul className="actions">
+        {" "}
+        <li>
+          <Link className="button" to="/register">
+            Register Here
+          </Link>
+        </li>{" "}
+        <li>
+          <Link className="button primary" to="/login">
+            Log In
+          </Link>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default Intro;
