9e827bdb8d3d3d78b24307b1bd88141eed197b38
Garrick Suemith <garrick.suemith@gmail.com>
2021-05-01 05:40:23 -0400

    data list

--- src/routes/recipes/components/AddRecipe.js
+++ src/routes/recipes/components/AddRecipe.js
@@ -26,7 +26,46 @@ const AddRecipe = () => {
 	const [ recipe, setRecipe ] = useState(initialState)
 	return (
 		<section>
-			<h2>Add your secret recipe</h2>
+		<section>
+		<h2>Add your secret recipe</h2>
+		
+			<form id="recipe-form">
+				<div className="row gtr-uniform gtr-50">
+					<div className="col-12">
+						<input
+							type="text"
+							name="title"
+							id="title"
+							placeholder="Recipe Title"
+							autoComplete="title"
+						/>
+					</div>
+					<div className="col-6 col-12-xsmall">
+						<input
+							type="list"
+							name="username"
+							id="username"
+							placeholder="Username"
+							autoComplete="username"
+						/>
+					</div>
+					<div className="col-6 col-12-xsmall">
+						<input
+							type="list"
+							name="username"
+							id="username"
+							placeholder="Username"
+							autoComplete="username"
+						/>
+					</div>
+				</div>
+			</form>
+			<ul className="actions">
+				<li>
+					<input type="submit" value="Submit Recipe" form="recipe-form"/>
+				</li>
+			</ul>
+		</section>
 		</section>
 	)
 }
