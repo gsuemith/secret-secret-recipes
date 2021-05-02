852ecdb20b729aec39f3592dbb43837932a75152
Garrick Suemith <garrick.suemith@gmail.com>
2021-05-02 00:53:32 -0400

    adding form functionality

--- src/routes/recipes/components/AddRecipe.js
+++ src/routes/recipes/components/AddRecipe.js
@@ -1,105 +1,115 @@
-import React, { useState, useEffect } from 'react'
-import RecipesList from '../RecipesList'
-import AddStep from './AddStep'
-import Ingredients from './Ingredients'
+import React, { useState, useEffect } from "react";
+import RecipesList from "../RecipesList";
+import AddStep from "./AddStep";
+import Ingredients from "./Ingredients";
 
 const initialState = {
-  "title": "",
-  "image_url": "/images/recipes/default.jpg",
-  "source": "",
-  "user_id": 1,
-  "categories": [],
-  "description": "",
-  "ingredients": [
+  title: "",
+  image_url: "/images/recipes/default.jpg",
+  source: "",
+  user_id: 1,
+  categories: [],
+  description: "",
+  ingredients: [
     {
-      "name": "water",
-      "quantity": 2,
-      "unit": "cups"
-    }
+      name: "water",
+      quantity: 2,
+      unit: "cups",
+    },
   ],
-  "steps": [
+  steps: [
     {
-      "step_number": 1,
-      "instructions": "Put water in microwave safe container and heat on high for 5 minutes or until boiling."
-    }
-  ]
-}
+      step_number: 1,
+      instructions:
+        "Put water in microwave safe container and heat on high for 5 minutes or until boiling.",
+    },
+  ],
+};
 
 const AddRecipe = () => {
-	const [ recipe, setRecipe ] = useState(initialState)
-	const [ categories, setCategories ] = useState(["one", "two", "three"])
-	const [ sources, setSources ] = useState(["four", "five", "six"])
+  const [recipe, setRecipe] = useState(initialState);
+  const [categories, setCategories] = useState(["one", "two", "three"]);
+  const [sources, setSources] = useState(["four", "five", "six"]);
 
-	return (
-		<section>
-		<section>
-		<h2>Add your secret recipe</h2>
-		
-			<form id="recipe-form">
-				<div className="row gtr-uniform gtr-50">
-					<div className="col-12">
-						<input
-							type="text"
-							name="title"
-							id="title"
-							placeholder="Recipe Title"
-							autoComplete="title"
-						/>
-					</div>
-					<div className="col-5 col-11-xsmall">
-						<input
-							list="categories"
-							name="category"
-							id="category"
-							placeholder="Choose a Category"
-							autoComplete="category"
-						/>
-					</div>
-					<div className="col-1">
-						<a href="\#" className="icon solid fa-plus">
-							<span class="label">label</span>
-						</a>
-					</div>
-					<div className="col-6 col-12-xsmall">
-						<input
-							list="sources"
-							name="source"
-							id="source"
-							placeholder="Source"
-							autoComplete="source"
-						/>
-					</div>
-					<div className="col-12">
-						<textarea 
-							name="description"
-							id="description"
-							placeholder="Description"
-						/>
-					</div>
-				</div>
-			</form>
-			<div className="row">
-				<div className="col-6 col-12-small">
-					<Ingredients ingredients={recipe.ingredients}/>
-				</div>
-				<div className="col-6 col-12-small">
-					<AddStep steps={recipe.steps}/>
-				</div>
-			</div>
-			<ul className="actions">
-				<li>
-					<input type="submit" value="Submit Recipe" form="recipe-form" className="primary"/>
-				</li>
-			</ul>
-		</section>
-			<datalist id="categories">
-				{categories.map(category => <option value={category} key={category}/>)}
-			</datalist>
-			<datalist id="sources">
-				{sources.map(source => <option value={source} key={source}/>)}
-			</datalist>
-		</section>
-	)
-}
+  return (
+    <section>
+      <section>
+        <h2>Add your secret recipe</h2>
 
-export default AddRecipe
+        <form id="recipe-form">
+          <div className="row gtr-uniform gtr-50">
+            <div className="col-12">
+              <input
+                type="text"
+                name="title"
+                id="title"
+                placeholder="Recipe Title"
+                autoComplete="title"
+              />
+            </div>
+            <div className="col-5 col-11-xsmall">
+              <input
+                list="categories"
+                name="category"
+                id="category"
+                placeholder="Choose a Category"
+                autoComplete="category"
+              />
+            </div>
+            <div className="col-1">
+              <a href="\#" className="icon solid fa-plus">
+                <span class="label">label</span>
+              </a>
+            </div>
+            <div className="col-6 col-12-xsmall">
+              <input
+                list="sources"
+                name="source"
+                id="source"
+                placeholder="Source"
+                autoComplete="source"
+              />
+            </div>
+            <div className="col-12">
+              <textarea
+                name="description"
+                id="description"
+                placeholder="Description"
+              />
+            </div>
+          </div>
+        </form>
+        <div className="row">
+          <div className="col-6 col-12-small">
+            <Ingredients recipe={recipe} update={setRecipe} />
+          </div>
+          <div className="col-6 col-12-small">
+            <AddStep steps={recipe.steps} />
+          </div>
+        </div>
+        <ul className="actions">
+          <li>
+            <input
+              type="submit"
+              value="Submit Recipe"
+              form="recipe-form"
+              className="primary"
+            />
+          </li>
+        </ul>
+      </section>
+      <datalist id="categories">
+        {categories.map((category) => (
+          <option value={category} key={category} />
+        ))}
+      </datalist>
+      <datalist id="sources">
+        {sources.map((source) => (
+          <option value={source} key={source} />
+        ))}
+      </datalist>
+    </section>
+  );
+};
+
+export default AddRecipe;
--- src/routes/recipes/components/Ingredients.js
+++ src/routes/recipes/components/Ingredients.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Ingredients = ({ ingredients }) => {
+const initialForm = {
+  name: "", quantity: 0, unit: ""
+}
+
+const Ingredients = ({ recipe, update }) => {
+  const { ingredients } = recipe;
+  const [ form, setForm ] = useState(initialForm)
+
+  const remove = (name) => {
+    update({
+      ...recipe,
+      ingredients: ingredients.filter(ingredient => {
+        return ingredient.name !== name
+      })
+    })
+  }
+
+  const add = ingredient => {
+    update({
+      ...recipe,
+      ingredients: [...ingredients, ingredient]
+    })
+  }
+
   return (
-    <div class="table-wrapper">
+  <div class="table-wrapper">
     <h3>Ingredients</h3>
     <table>
       <thead>
@@ -17,15 +40,15 @@ const Ingredients = ({ ingredients }) => {
           <td>{name}</td>
           <td>{quantity}</td>
           <td>{unit}</td>
-          <span onClick={e => console.log('x')} className="icon solid fa-times" style={{cursor: "pointer"}}></span>
+          <span onClick={() => remove(name)} className="icon solid fa-times" style={{cursor: "pointer"}}></span>
         </tr>))
       }
       </tbody>
     </table>
-    <form>
+    <form id="new-ingredient-form">
       <div className="row gtr-uniform gtr-50">
         <div className="col-4">
-          <input type="submit" value="Add Ingredient" className="button small"/>
+          <input form="new-ingredient-form" type="submit" value="Add Ingredient" className="button small"/>
         </div>
         <div className="col-12">
           <input type="text" placeholder="Ingredient Name"/>
